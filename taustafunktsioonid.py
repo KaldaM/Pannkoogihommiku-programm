4169cacc7eb57074efa006da6032e519be09a15c
KaldaM <matteuskalda@gmail.com>
2024-12-16 23:17:19 +0200

    Tegin tekstina salvestamise korda

--- taustafunktsioonid.py
+++ taustafunktsioonid.py
@@ -104,20 +104,66 @@ def uuenda_grupi_esemeid(sonastik):
 
 
 def koik_andmed_teksti(sonastik, failinimi):
-    print('Hello world')
     with open(failinimi, 'w', encoding='utf-8') as fail:
+        maaramata_grupp = False
+        maaramata_vool = 0
+        for punkt2, andmed2 in sonastik.items():
+            if 'grupp' in andmed2 and andmed2['grupp'] == '':
+                maaramata_grupp = True
+                maaramata_vool += andmed2['vooluvajadus']
+
+
         for grupp, andmed in sonastik.items():
             if 'grupp' not in andmed:
                 fail.write(f'Grupp: {grupp}\n')
-                fail.write(f'Grupi vooluvajadus {andmed["grupi vooluvajadus"]}\n')
-                fail.write(f'Gruppi kuuluvad punktid:\n')
+                fail.write(f'Grupi vooluvajadus: {andmed["grupi vooluvajadus"]}\n')
+                fail.write(f'Gruppi kuuluvad punktid:\n\n')
                 for punkt2, andmed2 in sonastik.items():
-                    if andmed2['grupp'] == grupp:
-                        fail.write(f'Nimi: {andmed2['nimi']}\n')
-                        fail.write(f'Elektrikapp: {andmed2['kapp']}\n')
+                    if 'grupp' in andmed2 and andmed2['grupp'] == grupp:
+                        if andmed2['nimi'] != '':
+                            fail.write(f'Nimi: {andmed2['nimi']}\n')
+                        else:
+                            fail.write(f'Nimi: {punkt2}\n')
+                        if andmed2['kapp'] != '':
+                            fail.write(f'Elektrikapp: {andmed2['kapp']}\n')
                         fail.write(f'Vooluvajadus kokku: {andmed2['vooluvajadus']}\n')
+                        if andmed2['seadmed'] != {}:
+                            fail.write(f'Seadmed:\n')
+                            for seade in andmed2['seadmed']:
+                                fail.write(f'   {seade}: {andmed2['seadmed'][seade]}W\n')
+                        if andmed2['kommentaar'] != '':
+                            fail.write(f'Kommentaarid:\n')
+                            read = andmed2['kommentaar'].split('\n')
+                            for rida in read:
+                                fail.write(f'   {rida.strip()}\n')
+                            fail.write('\n')
+                        else:
+                            fail.write('\n')
+
+                fail.write('--------------------------------------------\n\n')
+
+        if maaramata_grupp:
+            fail.write(f'Grupp: Määramata\n')
+            fail.write(f'Grupi vooluvajadus: {maaramata_vool}\n')
+            fail.write(f'Gruppi kuuluvad punktid:\n\n')
+            for punkt2, andmed2 in sonastik.items():
+                if 'grupp' in andmed2 and andmed2['grupp'] == '':
+                    if andmed2['nimi'] != '':
+                        fail.write(f'Nimi: {andmed2['nimi']}\n')
+                    else:
+                        fail.write(f'Nimi: {punkt2}\n')
+                    if 'kapp' in andmed2 and andmed2['kapp'] != '':
+                        fail.write(f'Elektrikapp: {andmed2['kapp']}\n')
+                    fail.write(f'Vooluvajadus kokku: {andmed2['vooluvajadus']}\n')
+                    if andmed2['seadmed'] != {}:
                         fail.write(f'Seadmed:\n')
                         for seade in andmed2['seadmed']:
-                            fail.write(f'{seade}: {andmed2['seadmed'][seade]}W\n')
+                            fail.write(f'   {seade}: {andmed2['seadmed'][seade]}W\n')
+                    if andmed2['kommentaar'] != '':
                         fail.write(f'Kommentaarid:\n')
-                        fail.write(f'{andmed2['kommentaar']}\n\n')
+                        read = andmed2['kommentaar'].split('\\\\\\')
+                        for rida in read:
+                            fail.write(f'   {rida.strip()}\n')
+                        fail.write('\n')
+                    else:
+                        fail.write('\n')
